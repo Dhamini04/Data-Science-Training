a7d28db2228c53bfe277396ef3f9f02b3a7b5712
dhaminimachireddy <dhaminimachireddy@gmail.com>
2025-07-18 18:13:44 +0530

    Update products-queries.js

--- Daily_Tasks/July18/products-queries.js
+++ Daily_Tasks/July18/products-queries.js
@@ -1,7 +1,17 @@
+/*
+Exercise: products Collection
+Step 1: Create and Insert Data into products
+use companyDB
+db.products.insertMany([
+{product_id: 101, name: "Laptop", category: "Electronics", price: 55000, stock: 10},
+{ product_id: 102, name: "Mouse", category: "Electronics", price: 700, stock: 50}, {product_id: 103, name: "Office Chair", category: "Furniture", price: 4500, stock:5},
+{ product_id: 104, name: "Notebook", category: "Stationery", price: 50, stock: 300},
+{ product_id: 105, name: "Water Bottle", category: "Kitchen", price: 250, stock: 100}])
+*/
+
 use companyDB
 switched to db companyDB
 use companyDB
-already on db companyDB
 db
 companyDB
 db.products.insertMany([
@@ -21,7 +31,11 @@ db.products.insertMany([
     '4': ObjectId('687a35be6028150f89160516')
   }
 }
+
+//1. Find all products that are not in the 'Electronics' category.
+
 db.products.find({category: {$ne : "Electronics"}})
+
 {
   _id: ObjectId('687a35be6028150f89160514'),
   product_id: 103,
@@ -46,7 +60,11 @@ db.products.find({category: {$ne : "Electronics"}})
   price: 250,
   stock: 100
 }
+
+//2. Get all products where price is greater than 1,000.
+
 db.products.find({price : {$gt :1000}})
+
 {
   _id: ObjectId('687a35be6028150f89160512'),
   product_id: 101,
@@ -55,32 +73,6 @@ db.products.find({price : {$gt :1000}})
   price: 55000,
   stock: 10
 }
-{
-  _id: ObjectId('687a35be6028150f89160514'),
-  product_id: 103,
-  name: 'Office Chair',
-  category: 'Furniture',
-  price: 4500,
-  stock: 5
-}
-db.products.find({stock: {$lt:50}})
-{
-  _id: ObjectId('687a35be6028150f89160512'),
-  product_id: 101,
-  name: 'Laptop',
-  category: 'Electronics',
-  price: 55000,
-  stock: 10
-}
-{
-  _id: ObjectId('687a35be6028150f89160514'),
-  product_id: 103,
-  name: 'Office Chair',
-  category: 'Furniture',
-  price: 4500,
-  stock: 5
-}
-db.products.find({category: {$in:["Furniture","Kitchen"]}})
 {
   _id: ObjectId('687a35be6028150f89160514'),
   product_id: 103,
@@ -90,7 +82,44 @@ db.products.find({category: {$in:["Furniture","Kitchen"]}})
   stock: 5
 }
 
+//3. Find products that have stock less than 50.
+
+db.products.find({stock: {$lt:50}})
+
+{
+  _id: ObjectId('687a35be6028150f89160512'),
+  product_id: 101,
+  name: 'Laptop',
+  category: 'Electronics',
+  price: 55000,
+  stock: 10
+}
+{
+  _id: ObjectId('687a35be6028150f89160514'),
+  product_id: 103,
+  name: 'Office Chair',
+  category: 'Furniture',
+  price: 4500,
+  stock: 5
+}
+
+//4. List products whose category is either 'Furniture' or 'Kitchen'.
+
+db.products.find({category: {$in:["Furniture","Kitchen"]}})
+
+{
+  _id: ObjectId('687a35be6028150f89160514'),
+  product_id: 103,
+  name: 'Office Chair',
+  category: 'Furniture',
+  price: 4500,
+  stock: 5
+}
+
+//5. Get products with stock between 10 and 100.
+
 db.products.find({stock: {$gte:10, $lte:100}})
+
 {
   _id: ObjectId('687a35be6028150f89160512'),
   product_id: 101,
@@ -115,7 +144,11 @@ db.products.find({stock: {$gte:10, $lte:100}})
   price: 250,
   stock: 100
 }
+
+//6. Find all products where price is not 700.
+
 db.products.find({price: {$ne: 700}})
+
 {
   _id: ObjectId('687a35be6028150f89160512'),
   product_id: 101,
@@ -149,7 +182,10 @@ db.products.find({price: {$ne: 700}})
   stock: 100
 }
 
+//7. Display all products whose name starts with 'N'.
+
 db.products.find({ name: { $regex: /^N/ } })
+
 {
   _id: ObjectId('687a35be6028150f89160515'),
   product_id: 104,
@@ -158,6 +194,9 @@ db.products.find({ name: { $regex: /^N/ } })
   price: 50,
   stock: 300
 }
+
+//8 . Find all products whose stock is not more than 5.
+
 db.products.find({stock:{$lte: 5}})
 {
   _id: ObjectId('687a35be6028150f89160514'),
@@ -167,7 +206,11 @@ db.products.find({stock:{$lte: 5}})
   price: 4500,
   stock: 5
 }
+
+//9. List products with category not in ['Stationery', 'Kitchen'].
+
 db.products.find({category: {$in: ["Stationery","Kitchen"]}})
+
 {
   _id: ObjectId('687a35be6028150f89160515'),
   product_id: 104,
@@ -176,7 +219,11 @@ db.products.find({category: {$in: ["Stationery","Kitchen"]}})
   price: 50,
   stock: 300
 }
+
+//10.Find one product not in the 'Furniture' category.
+
 db.products.find({category: {$ne: "Furniture"}})
+
 {
   _id: ObjectId('687a35be6028150f89160512'),
   product_id: 101,
