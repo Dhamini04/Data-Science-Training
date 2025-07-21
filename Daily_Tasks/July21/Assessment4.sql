26f254e78d233a298f3784689dfedceb823f4ce1
dhaminimachireddy <dhaminimachireddy@gmail.com>
2025-07-21 17:01:34 +0530

    Update Assessment4.sql

--- Daily_Tasks/July21/Assessment4.sql
+++ Daily_Tasks/July21/Assessment4.sql
@@ -1,3 +1,26 @@
+--------------------SQL Case Study Assessment: Movie Rentals System---------------------------------------
+/*SECTION 1: Database Design
+(Create tables with proper datatypes & keys) Design the schema with these 3 tables:
+1. movies
+movie_id (Primary Key)
+title
+genre
+release_year
+rental_rate
+2. customers
+customer_id (Primary Key)
+name
+email
+city
+3. rentals
+rental_id (Primary Key)
+customer_id (Foreign Key)
+movie_id (Foreign Key)
+rental_date
+return_date
+Task: Write SQL commands to create these tables using appropriate data types and
+constraints.*/
+
 -- Movies table
 CREATE TABLE movies (
   movie_id INT PRIMARY KEY,
@@ -15,6 +38,8 @@ CREATE TABLE customers (
   city VARCHAR(50)
 );
 
+--rentals table
+
 CREATE TABLE rentals (
   rental_id INT PRIMARY KEY,
   customer_id INT,
@@ -24,6 +49,15 @@ CREATE TABLE rentals (
   FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
   FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
 );
+
+
+/*SECTION 2: Data Insertion
+Insert sample records into each table:
+At least 5 movies from different genres and years
+At least 5 customers from different cities
+At least 8 rental records — make sure at least one movie is rented more than
+once
+ Task: Write SQL INSERT statements for all the data.*/
 
 INSERT INTO movies VALUES
 (1, 'Inception', 'Sci-Fi', 2010, 150),
@@ -49,6 +83,11 @@ INSERT INTO rentals VALUES
 (1007, 102, 5, '2023-06-10', '2023-06-13'),
 (1008, 103, 1, '2023-07-01', '2023-07-04');
 
+----SECTION 3: Query Execution---------------------
+-- Basic Queries
+
+--1. Retrieve all movies rented by a customer named 'Amit Sharma'.
+
 SELECT m.title, c.name
 FROM rentals r
 JOIN customers c
@@ -57,17 +96,27 @@ JOIN movies m
 ON r.movie_id = m.movie_id
 WHERE c.name = 'Amit Singh';
 
+--2. Show the details of customers from 'Bangalore'.
+
 SELECT * FROM  customers
 WHERE city ='BANGALORE';
- 
+
+ --3. List all movies released after the year 2020.
+
 SELECT * FROM movies
 WHERE release_year > 2020;
+
+---------------Aggregate Queries----------------------
+
+--4. Count how many movies each customer has rented.
 
 SELECT c.name, COUNT(r.rental_id) AS total_rented 
 FROM customers c
 JOIN rentals r
 ON c.customer_id = r.customer_id
 GROUP BY c.name;
+
+--5. Find the most rented movie title.
 
 SELECT m.title , COUNT(rental_id) AS rental_count
 FROM movies m
@@ -77,15 +126,20 @@ GROUP BY m.title
 ORDER BY rental_count DESC
 limit 1;
 
+--6. Calculate total revenue earned from all rentals.
+
 SELECT SUM(m.rental_rate) as total_revenue
 FROM rentals r
 JOIN movies m
 ON r.movie_id = m.movie_id;
 
+--7. List all customers who have never rented a movie.
+
 SELECT name
 FROM customers 
 WHERE customer_id NOT IN (SELECT DISTINCT  customer_id FROM rentals);
 
+--8. Show each genre and the total revenue from that genre.
 
 SELECT m.genre, SUM(m.rental_rate) as genre_revenue
 FROM rentals r
@@ -93,6 +147,7 @@ JOIN movies m
 ON m.movie_id = r.movie_id
 GROUP BY m.genre;
 
+--9. Find the customer who spent the most money on rentals.
 
 SELECT c.name, SUM(m.rental_rate) AS total_spent
 FROM customers c
@@ -103,6 +158,8 @@ ON r.movie_id = m.movie_id
 GROUP BY c.name
 ORDER BY total_spent DESC
 LIMIT 1;
+
+--10. Display movie titles that were rented and not yet returned ( return_date IS NULL ).
 
 SELECT m.title, r.rental_date
 FROM rentals r
