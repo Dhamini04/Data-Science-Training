e59e66a1a75b226612c7df177161cf5ac92460ad
dhaminimachireddy <dhaminimachireddy@gmail.com>
2025-07-22 16:37:45 +0530

    Update MongoDB Assessment 4.js

--- Daily_Tasks/JULY 22/MongoDB Assessment 4.js
+++ Daily_Tasks/JULY 22/MongoDB Assessment 4.js
@@ -1,5 +1,24 @@
+//MongoDB Assignment: Fitness Center Database
+//Step 1: Create Collections and Insert Data
+
 use FitnessCenterDB
 switched to db FitnessCenterDB
+
+
+/*members
+db.members.insertMany([
+{ member_id: 1, name: "Anjali Rao", age: 28, gender: "Female", city: "Mumbai",
+membership_type: "Gold" },
+{ member_id: 2, name: "Rohan Mehta", age: 35, gender: "Male", city: "Delhi",
+membership_type: "Silver" },
+{ member_id: 3, name: "Fatima Shaikh", age: 22, gender: "Female", city: "Hyderabad",
+membership_type: "Platinum" },
+{ member_id: 4, name: "Vikram Das", age: 41, gender: "Male", city: "Bangalore",
+membership_type: "Gold" },
+{ member_id: 5, name: "Neha Kapoor", age: 31, gender: "Female", city: "Pune",
+membership_type: "Silver" }
+])*/
+
 db.members.insertMany([
 { member_id: 1, name: "Anjali Rao", age: 28, gender: "Female", city: "Mumbai",
 membership_type: "Gold" },
@@ -22,6 +41,17 @@ membership_type: "Silver" }
     '4': ObjectId('687f68b35dabfa37b04571be')
   }
 }
+
+/*
+trainers
+db.trainers.insertMany([
+{ trainer_id: 101, name: "Ajay Kumar", specialty: "Weight Training", experience: 7
+},
+{ trainer_id: 102, name: "Swati Nair", specialty: "Cardio", experience: 5 },
+{ trainer_id: 103, name: "Imran Qureshi", specialty: "Yoga", experience: 8 }
+])
+*/
+
 db.trainers.insertMany([
 { trainer_id: 101, name: "Ajay Kumar", specialty: "Weight Training", experience: 7
 },
@@ -36,6 +66,24 @@ db.trainers.insertMany([
     '2': ObjectId('687f68bd5dabfa37b04571c1')
   }
 }
+
+/*
+sessions
+db.sessions.insertMany([
+{ session_id: 201, member_id: 1, trainer_id: 101, session_type: "Strength",
+duration: 60, date: new Date("2024-08-01") },
+{ session_id: 202, member_id: 2, trainer_id: 102, session_type: "Cardio", duration:
+45, date: new Date("2024-08-02") },
+{ session_id: 203, member_id: 3, trainer_id: 103, session_type: "Yoga", duration:
+90, date: new Date("2024-08-03") },
+{ session_id: 204, member_id: 1, trainer_id: 102, session_type: "Cardio", duration:
+30, date: new Date("2024-08-04") },
+{ session_id: 205, member_id: 4, trainer_id: 101, session_type: "Strength",
+duration: 75, date: new Date("2024-08-05") },
+{ session_id: 206, member_id: 5, trainer_id: 103, session_type: "Yoga", duration:
+60, date: new Date("2024-08-05") }
+])
+*/
 db.sessions.insertMany([
 { session_id: 201, member_id: 1, trainer_id: 101, session_type: "Strength",
 duration: 60, date: new Date("2024-08-01") },
@@ -61,6 +109,11 @@ duration: 75, date: new Date("2024-08-05") },
     '5': ObjectId('687f68c75dabfa37b04571c7')
   }
 }
+
+//Step 2: Long List of Query Challenges
+//Basic Queries
+//1. Find all members from Mumbai.
+
 db.members.find({ city: "Mumbai" });
 {
   _id: ObjectId('687f68b35dabfa37b04571ba'),
@@ -71,6 +124,9 @@ db.members.find({ city: "Mumbai" });
   city: 'Mumbai',
   membership_type: 'Gold'
 }
+
+//2. List all trainers with experience greater than 6 years.
+
 db.trainers.find({ experience: { $gt: 6 } });
 {
   _id: ObjectId('687f68bd5dabfa37b04571bf'),
@@ -86,6 +142,9 @@ db.trainers.find({ experience: { $gt: 6 } });
   specialty: 'Yoga',
   experience: 8
 }
+
+//3. Get all Yoga sessions.
+
 db.sessions.find({ session_type: "Yoga" });
 {
   _id: ObjectId('687f68c75dabfa37b04571c4'),
@@ -105,6 +164,9 @@ db.sessions.find({ session_type: "Yoga" });
   duration: 60,
   date: 2024-08-05T00:00:00.000Z
 }
+
+// 4. Show all sessions conducted by trainer Swati Nair.
+
 db.sessions.aggregate([
   { $lookup: {
       from: "trainers",
@@ -147,10 +209,18 @@ db.sessions.aggregate([
     experience: 5
   }
 }
+
+//5. Find all members who attended a session on 2024-08-05.
+
 db.sessions.find({ date: ISODate("2024-08-05") })
   .forEach(s => print(s.member_id));
 4
 5
+
+
+//Intermediate Queries
+//6. Count the number of sessions each member has attended.*/
+
 db.sessions.aggregate([
   { $group: {
       _id: "$member_id",
@@ -177,6 +247,10 @@ db.sessions.aggregate([
   _id: 3,
   session_count: 1
 }
+
+
+//7. Show average duration by session_type
+
 db.sessions.aggregate([
   { $group: {
       _id: "$session_type",
@@ -195,6 +269,10 @@ db.sessions.aggregate([
   _id: 'Cardio',
   avg_duration: 37.5
 }
+
+
+//8. Find all female members who attended a session longer than 60 minutes.
+
 db.sessions.aggregate([
   { $match: { duration: { $gt: 60 } } },
   { $lookup: {
@@ -224,6 +302,10 @@ db.sessions.aggregate([
     membership_type: 'Platinum'
   }
 }
+
+
+//9. Display sessions sorted by duration (descending).
+
 db.sessions.find().sort({ duration: -1 });
 {
   _id: ObjectId('687f68c75dabfa37b04571c4'),
@@ -279,6 +361,10 @@ db.sessions.find().sort({ duration: -1 });
   duration: 30,
   date: 2024-08-04T00:00:00.000Z
 }
+
+
+//10. Find members who have attended sessions with more than one trainer.
+
 db.sessions.aggregate([
   { $group: {
       _id: "$member_id",
@@ -293,6 +379,11 @@ db.sessions.aggregate([
   _id: 1,
   trainer_count: 2
 }
+
+
+/* Advanced Queries with Aggregation & Lookup
+//11. Use $lookup to display sessions with member name and trainer name.*/
+
 db.sessions.aggregate([
   { $lookup: {
       from: "members",
@@ -364,6 +455,9 @@ db.sessions.aggregate([
   member_name: 'Neha Kapoor',
   trainer_name: 'Imran Qureshi'
 }
+
+
+//12. Calculate total session time per trainer
 db.sessions.aggregate([
   { $group: {
       _id: "$trainer_id",
@@ -382,6 +476,9 @@ db.sessions.aggregate([
   _id: 102,
   total_time: 75
 }
+
+//13. List total time spent per member.
+
 db.sessions.aggregate([
   { $group: {
       _id: "$member_id",
@@ -408,6 +505,9 @@ db.sessions.aggregate([
   _id: 2,
   total_time: 45
 }
+
+//14. Count sessions per trainer
+
 db.sessions.aggregate([
   { $group: {
       _id: "$trainer_id",
@@ -426,6 +526,9 @@ db.sessions.aggregate([
   _id: 101,
   session_count: 2
 }
+
+//15. Trainer with longest average session duration
+
 db.sessions.aggregate([
   { $group: {
       _id: "$trainer_id",
@@ -438,6 +541,9 @@ db.sessions.aggregate([
   _id: 103,
   avg_duration: 75
 }
+
+//16. Unique members per trainer
+
 db.sessions.aggregate([
   { $group: {
       _id: "$trainer_id",
@@ -459,6 +565,9 @@ db.sessions.aggregate([
   _id: 101,
   member_count: 2
 }
+
+//17. Find most active member by total duration
+
 db.sessions.aggregate([
   { $group: {
       _id: "$member_id",
@@ -471,6 +580,9 @@ db.sessions.aggregate([
   _id: 3,
   total_time: 90
 }
+
+//18. List all Gold membership members who took at least one Strength session.
+
 db.sessions.aggregate([
   { $match: { session_type: "Strength" } },
   { $lookup: {
@@ -499,6 +611,9 @@ db.sessions.aggregate([
     name: 'Vikram Das'
   }
 }
+
+//19. Display a breakdown of sessions by membership type. 
+
 db.sessions.aggregate([
   { $lookup: {
       from: "members",
@@ -524,6 +639,9 @@ db.sessions.aggregate([
   _id: 'Platinum',
   session_count: 1
 }
+
+//20. Find members who have not attended any session yet (hint: simulate later byadding a new member).
+
 db.members.insertOne({
   member_id: 6, name: "Samir Patel",
   age: 29, gender: "Male", city: "Ahmedabad",
